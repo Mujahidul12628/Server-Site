10fc4928a6bef23c68dd4df764f18a7aafe13b8a
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-05-01 12:26:19 +0600

    Google popup added

---
+++ firebase-primary-project/src/assets/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDsvw3F8pdl3wG9l5EfcI5nn-QHRsDiu9o",
+    authDomain: "simple-firebase-39153.firebaseapp.com",
+    projectId: "simple-firebase-39153",
+    storageBucket: "simple-firebase-39153.appspot.com",
+    messagingSenderId: "494242737032",
+    appId: "1:494242737032:web:89ce04300884c9c6ce283d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- firebase-primary-project/src/main.jsx
+++ firebase-primary-project/src/main.jsx
@@ -6,11 +6,24 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Main from './assets/layout/Main.jsx';
+import Home from './assets/components/Home/Home.jsx';
+import Login from './assets/components/Login/Login.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+    ]
   },
 ]);
 
